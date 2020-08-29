1884e19c8406cbf2319f81a6a10c1f3ac4fabc18
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2020-08-29 15:26:38 +0300

    учеба

--- buf.go
+++ buf.go
@@ -6,11 +6,15 @@ import (
 	"os"
 )
 
+type myFile struct {
+	counts map[string]int
+}
+
 func main() {
-	counts := make(map[string]int)
+	myFiles := make(map[string]myFile)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, myFiles, "Stdin")
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -18,22 +22,29 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, myFiles, arg)
 			f.Close()
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+
+	for name, myFile := range myFiles {
+		fmt.Println(name)
+		for line, n := range myFile.counts {
+			if n > 1 {
+				fmt.Printf("%d\t%s\n", n, line)
+			}
 		}
 	}
 }
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, myFiles map[string]myFile, filename string) {
 	input := bufio.NewScanner(f)
+	file := new(myFile)
+	file.counts = make(map[string]int)
 	for input.Scan() {
 		if input.Text() == "" {
 			break
 		}
-		counts[input.Text()]++
+		file.counts[input.Text()]++
+		myFiles[filename] = *file
 	}
 }
