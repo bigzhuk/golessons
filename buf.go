054382f8d31f9214721603e10f5e32c1f36e493d
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2020-08-29 12:37:54 +0300

    asds

--- buf.go
+++ buf.go
@@ -8,10 +8,32 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
-		fmt.Println(counts)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
 	}
-
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		if input.Text() == "" {
+			break
+		}
+		counts[input.Text()]++
+	}
 }
